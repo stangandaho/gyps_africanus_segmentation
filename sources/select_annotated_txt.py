4f0f4bb00f8f2e4de10b024428849511a35379ce
@stangandaho <gandahostanmah@gmail.com>
2024-01-03 21:07:27 +0100

    Wrap in function

--- sources/select_annotated_txt.py
+++ sources/select_annotated_txt.py
@@ -4,55 +4,61 @@ from ultralytics.data.converter import convert_coco
 
 ## Root for coco format files
 annot_train_dir = "./annotation/train"
-annot_val_root = "./annotation/val"
+annot_val_dir = "./annotation/val"
+
 ## Create directories for split
 split_dir = ["train", "val", "test"]
 for dr in split_dir:
     Path("./dataset", "images", dr).mkdir(exist_ok=True, parents=True)
-for dr in split_dir:
     Path("./dataset", "labels", dr).mkdir(exist_ok=True, parents=True)
 
+## Function to onverte coco format to text
+def cf_to_text(coco_dir: str, dest_dir:str):
+    """Convert coco format to text file
 
-## Convert coco format to .txt - Training label
-if Path("./coco_converted").exists():
-    shutil.rmtree(Path("./coco_converted"))
+    Args:
+        coco_dir (str): the directory that contain coco format
+        dest_dir (str): the destination directory for text file
+    """
 
-convert_coco(labels_dir = str(annot_val_root),  use_segments=True)
+    if Path("./coco_converted").exists():
+        shutil.rmtree(Path("./coco_converted"))
+    convert_coco(labels_dir = str(coco_dir),  use_segments=True)
 
-lbl_path = Path("./coco_converted/labels/default")
-for lbl in lbl_path.iterdir():
-    shutil.copy(lbl, Path("./dataset/labels/train", lbl.name))
+    lbl_path = Path("./coco_converted/labels/default")
+    for lbl in lbl_path.iterdir():
+        shutil.copy(lbl, Path(dest_dir, lbl.name))
 
-## Populate images/train
-train_lbl_dir = Path("./dataset/labels/train")
-train_image_src = [x for x in Path("./image_src/train").iterdir()]
+## Function to pupulate images/train; images/val
+def populate_imfolder(image_src:str, label_dir:str, dest_dir:str):
+    """Populate the train or validation directory basing on label files.
 
-for tr in train_lbl_dir.iterdir():
-     to_copy_name , to_copy_ext = tr.stem, tr.suffix
-     for j in train_image_src:
-          if to_copy_name in str(j):
-               new_file = Path("./dataset/images/train", j.name)
-               shutil.copy(j, new_file)
+    Args:
+        image_src (str): the directory where to copy images from
+        label_dir (str): the directory that contains label file (.txt), ideally the output of `cf_to_text`
+        dest_dir (str): the destination directory for image relatives to each text file
+    """
+
+    train_lbl_dir = Path(label_dir)
+    train_image_src = [x for x in Path(image_src).iterdir()]
+
+    for tr in train_lbl_dir.iterdir():
+        to_copy_name = tr.stem
+        for j in train_image_src:
+            if to_copy_name in str(j):
+                new_file = Path(dest_dir, j.name)
+                shutil.copy(j, new_file)
+     
+
+# Convert coco format to .txt --> Training label
+cf_to_text(coco_dir=annot_train_dir, dest_dir="./dataset/labels/train")
+# Convert coco format to .txt --> Validation label
+cf_to_text(coco_dir=annot_val_dir, dest_dir="./dataset/labels/val")
 
 
-## Convert coco format to .txt - Val label
-if Path("./coco_converted").exists():
-    shutil.rmtree(Path("./coco_converted"))
-
-convert_coco(labels_dir = str(annot_val_root),  use_segments=True)
-## Copy txt file to appropriate folder
-lbl_path = Path("./coco_converted/labels/default")
-for lbl in lbl_path.iterdir():
-    shutil.copy(lbl, Path("./dataset/labels/val", lbl.name))
-
-## Populate images/train
-val_lbl_dir = Path("./dataset/labels/val")
-val_image_src = [x for x in Path("./image_src/val").iterdir()]
-
-for tr in val_lbl_dir.iterdir():
-     to_copy_name , to_copy_ext = tr.stem, tr.suffix
-     for j in val_image_src:
-          if to_copy_name in str(j):
-               new_file = Path("./dataset/images/val", j.name)
-               shutil.copy(j, new_file)
-
+## Populate images/train folder basing on label files
+populate_imfolder(image_src = "./image_src/train", label_dir="./dataset/labels/train", \
+                dest_dir = "./dataset/images/train")
+## Populate images/val folder basing on label files
+populate_imfolder(image_src = "./image_src/val", label_dir="./dataset/labels/val", \
+                dest_dir = "./dataset/images/val")
